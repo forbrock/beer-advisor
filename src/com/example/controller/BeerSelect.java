66d2a59a7f7bb0531a3fa6fb9f9129c37d6b982f
forbrock <badgeon@gmail.com>
2020-10-30 18:42:34 +0200

    added project files

---
+++ src/com/example/controller/BeerSelect.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import com.example.model.BeerExpert;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.List;
+import java.util.Iterator;
+
+public class BeerSelect extends HttpServlet {
+	
+	public void doPost(HttpServletRequest request,
+					HttpServletResponse response)
+					throws IOException, ServletException {
+		
+		response.setContentType("text/html; charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+				
+		String color = request.getParameter("color");
+		BeerExpert be = new BeerExpert();
+		List result = be.getBrands(color);
+		
+		PrintWriter out = response.getWriter();
+		out.println("Beer selection advice:<br>");
+		
+		Iterator it = result.iterator();
+		while (it.hasNext()) {
+			out.print("<br>try: " + it.next());
+		}
+	}
+}
